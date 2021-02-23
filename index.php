240e4f07f83fb42f37d8a41fe321faed7bd3516c
SanjeQi <cristi.nord@gmail.com>
2021-02-23 21:01:01 +0000

    Php form play initial setup

--- index.php
+++ index.php
@@ -1,39 +1,78 @@
+<?php
+
+if($_POST["submit"]) {
+  $result='<div class="alert alert-success"> Form submitted </div>';
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Php</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Latest compiled and minified CSS -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="style.css">
   </head>
+
   <body>
-
-<?php 
-
-$names = array ("Fred", "Rob","Ian");
-
-if ($_POST["submit"]) {
-  if ($_POST["name"]) {
-   foreach ($names as $name) {
-		if ($_POST["name"] == $name) {
-			echo "I know you! Your name is".$name;
-			$knowYou = 1;
-		} 
-	}
-	if (!$knowYou) echo "I don't know you";
-
-  } else {
-    echo "Please enter your name";
-  }
-
-}
-?>
-
-    <form>
-      <label for="name">Name</label>
-      <input name="name" type="text" />
-      <input type="submit" name="submit" value="Submit your name now" />
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor ipsam quos sequi vel. Vero doloribus quo quas, esse maiores cumque?</p>
-    <form>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3 emailForm">
+          <h1>Contact</h1>
+          <?php echo $result ?>
+          <p class="lead para">
+            Please get in touch  I'll get back to you as soon as I can.
+          </p>
+          <form class="my_form" method="post">
+            <div class="form-group">
+              <label for="name">Your Name:</label>
+              <input
+                type="text"
+                name="name"
+                class="form-control"
+                placeholder="Your Name"
+                
+              />
+            </div>
+            <div class="form-group">
+              <label for="email">Your Email:</label>
+              <input
+                type="email"
+                name="email"
+                class="form-control"
+                placeholder="Your Email"
+                
+              />
+            </div>
+            <div class="form-group">
+              <label for="comment">Your Comment:</label>
+              <textarea class="form-control" name="comment"> </textarea>
+            </div>
+            <input
+              type="submit"
+              name="submit"
+              class="btn btn-warning btn-lg"
+              value="Submit"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+    <!-- Latest compiled and minified JavaScript -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
+      crossorigin="anonymous"
+    ></script>
   </body>
 </html>
